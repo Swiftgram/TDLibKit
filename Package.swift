16319fd86251c9a4a549e7000c82cc203d67e5d2
Sergey <iceman9831@gmail.com>
2021-09-02 20:17:00 +0300

    Bump tdlibframework 1.7.7-7135caa

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
             targets: ["TDLibKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Swiftgram/TDLibFramework", .upToNextMinor(from: "1.7.6-9e7bce1")),
+        .package(url: "https://github.com/Swiftgram/TDLibFramework", .upToNextMinor(from: "1.7.7-7135caa")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
