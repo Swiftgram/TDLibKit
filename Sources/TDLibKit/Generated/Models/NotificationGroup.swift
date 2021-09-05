41fed9c8079c07a3616e9a3f69b988b291495e59
runner <runner@Mac-1630826453487.local>
2021-09-05 07:44:45 +0000

    [no ci] Version 1.0.0-tdlib-1.7.7-b713d945

--- Sources/TDLibKit/Generated/Models/NotificationGroup.swift
+++ Sources/TDLibKit/Generated/Models/NotificationGroup.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.7.7-7135caa
-//  https://github.com/tdlib/td/tree/7135caa
+//  Based on TDLib 1.7.7-b713d945
+//  https://github.com/tdlib/td/tree/b713d945
 //
 
 import Foundation
