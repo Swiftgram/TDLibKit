27895bf4bd4f3604dbee824fc0c3e424059a3e02
runner <runner@Mac-1633677572918.local>
2021-10-08 07:55:06 +0000

    [no ci] Version 1.0.0-tdlib-1.7.8-bbae7be4

--- Sources/TDLibKit/Generated/Models/IdentityDocument.swift
+++ Sources/TDLibKit/Generated/Models/IdentityDocument.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.7.8-5f19e026
-//  https://github.com/tdlib/td/tree/5f19e026
+//  Based on TDLib 1.7.8-bbae7be4
+//  https://github.com/tdlib/td/tree/bbae7be4
 //
 
 import Foundation
