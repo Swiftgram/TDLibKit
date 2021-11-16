1b024668841982ab479e2ce7fd382886e050157f
runner <runner@Mac-1637060785745.local>
2021-11-16 12:13:04 +0000

    [no ci] Version 1.1.0-tdlib-1.7.9-c2c49ebc

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             targets: ["TDLibKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.7.9-33c689aa")),
+        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.7.9-c2c49ebc")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/TDLibKit/Generated/Models/LeaveChat.swift
+++ Sources/TDLibKit/Generated/Models/LeaveChat.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.7.9-33c689aa
-//  https://github.com/tdlib/td/tree/33c689aa
+//  Based on TDLib 1.7.9-c2c49ebc
+//  https://github.com/tdlib/td/tree/c2c49ebc
 //
 
 import Foundation
