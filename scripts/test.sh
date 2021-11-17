24a5a654d9601be91c9d9661758d74cb6d44e56e
Sergey <iceman9831@gmail.com>
2021-11-17 19:34:42 +0300

    Integrate tests

---
+++ scripts/test.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+
+PLATFORM="$1"
+
+
+if [[ $PLATFORM = "iOS-simulator" ]]; then
+    SDK="iphonesimulator"
+    SCHEME="iOSApp"
+    DESTINATION='platform=iOS Simulator,name=iPhone 13 Pro'
+elif [[ $PLATFORM = "macOS" ]]; then
+    SDK="macosx"
+    SCHEME="macOSApp"
+    DESTINATION='platform=OS X'
+elif [[ $PLATFORM = "watchOS-simulator" ]]; then
+    SDK="watchsimulator"
+    SCHEME="watchOSApp"
+    DESTINATION='platform=watchOS Simulator,name=watch OS'
+elif [[ $PLATFORM = "tvOS-simulator" ]]; then
+    SDK="appletvsimulator"
+    SCHEME="tvOSApp"
+    DESTINATION='platform=tvOS Simulator,name=Apple TV' 
+else
+    echo "Unknown SDK for platform \"$PLATFORM\""
+    exit 1
+fi
+
+cd Tests/Apps
+
+xcodebuild \
+  -scheme ${SCHEME} \
+  -sdk ${SDK} \
+  -destination "${DESTINATION}" \
+  clean test
