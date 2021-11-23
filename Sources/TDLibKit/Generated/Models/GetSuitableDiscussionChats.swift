e90cc7c0dd4110d4e653a7aea673c0b903c5d79c
runner <runner@Mac-1637669255231.local>
2021-11-23 12:15:53 +0000

    [no ci] Version 1.1.0-tdlib-1.7.9-0c554fbe

--- Sources/TDLibKit/Generated/Models/GetSuitableDiscussionChats.swift
+++ Sources/TDLibKit/Generated/Models/GetSuitableDiscussionChats.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.7.9-743a30d4
-//  https://github.com/tdlib/td/tree/743a30d4
+//  Based on TDLib 1.7.9-0c554fbe
+//  https://github.com/tdlib/td/tree/0c554fbe
 //
 
 import Foundation
