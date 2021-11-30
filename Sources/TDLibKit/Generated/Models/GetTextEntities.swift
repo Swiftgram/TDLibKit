67a5d8da142acce0cc521237c97806064f0bb7f4
runner <runner@Mac-1638231793028.local>
2021-11-30 00:31:00 +0000

    [no ci] Version 1.1.0-tdlib-1.7.9-e79ec946

--- Sources/TDLibKit/Generated/Models/GetTextEntities.swift
+++ Sources/TDLibKit/Generated/Models/GetTextEntities.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.7.9-2725f7c5
-//  https://github.com/tdlib/td/tree/2725f7c5
+//  Based on TDLib 1.7.9-e79ec946
+//  https://github.com/tdlib/td/tree/e79ec946
 //
 
 import Foundation
