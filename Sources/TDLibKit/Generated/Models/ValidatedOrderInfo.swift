7cb33efa7aa66a6f7f98f6678a36f4ee90128066
runner <runner@Mac-1638312569384.local>
2021-12-01 00:29:49 +0000

    [no ci] Version 1.1.0-tdlib-1.7.9-5dc5b473

--- Sources/TDLibKit/Generated/Models/ValidatedOrderInfo.swift
+++ Sources/TDLibKit/Generated/Models/ValidatedOrderInfo.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.7.9-e79ec946
-//  https://github.com/tdlib/td/tree/e79ec946
+//  Based on TDLib 1.7.9-5dc5b473
+//  https://github.com/tdlib/td/tree/5dc5b473
 //
 
 import Foundation
