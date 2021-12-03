4f60e0b050298fa73b718cd022ec2aa9879d1a7c
runner <runner@Mac-1638491086980.local>
2021-12-03 00:29:38 +0000

    [no ci] Version 1.1.0-tdlib-1.7.9-1d3cf2c2

--- Sources/TDLibKit/Generated/Models/WebPageInstantView.swift
+++ Sources/TDLibKit/Generated/Models/WebPageInstantView.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.7.9-5dc5b473
-//  https://github.com/tdlib/td/tree/5dc5b473
+//  Based on TDLib 1.7.9-1d3cf2c2
+//  https://github.com/tdlib/td/tree/1d3cf2c2
 //
 
 import Foundation
