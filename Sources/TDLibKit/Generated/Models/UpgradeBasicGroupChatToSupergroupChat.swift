d421d2c58c445f2944b5766d8555b227c8e0d1ad
runner <runner@Mac-1638792608882.local>
2021-12-06 12:13:20 +0000

    [no ci] Version 1.1.0-tdlib-1.7.9-858078d8

--- Sources/TDLibKit/Generated/Models/UpgradeBasicGroupChatToSupergroupChat.swift
+++ Sources/TDLibKit/Generated/Models/UpgradeBasicGroupChatToSupergroupChat.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.7.9-8d7bda00
-//  https://github.com/tdlib/td/tree/8d7bda00
+//  Based on TDLib 1.7.9-858078d8
+//  https://github.com/tdlib/td/tree/858078d8
 //
 
 import Foundation
