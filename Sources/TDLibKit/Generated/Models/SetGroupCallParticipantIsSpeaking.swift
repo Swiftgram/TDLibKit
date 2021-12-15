f6e8ec56b723408386fd4eab854beef3f9cb7026
runner <runner@Mac-1639570214800.local>
2021-12-15 12:13:36 +0000

    [no ci] Version 1.1.0-tdlib-1.7.10-fdcb1098

--- Sources/TDLibKit/Generated/Models/SetGroupCallParticipantIsSpeaking.swift
+++ Sources/TDLibKit/Generated/Models/SetGroupCallParticipantIsSpeaking.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.7.10-d437ad27
-//  https://github.com/tdlib/td/tree/d437ad27
+//  Based on TDLib 1.7.10-fdcb1098
+//  https://github.com/tdlib/td/tree/fdcb1098
 //
 
 import Foundation
