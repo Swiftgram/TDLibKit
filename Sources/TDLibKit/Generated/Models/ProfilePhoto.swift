9aae4f3c5409d62ab0f8707297b9b5fdd0474d68
runner <runner@Mac-1640174771379.local>
2021-12-22 12:15:10 +0000

    [no ci] Version 1.1.0-tdlib-1.7.10-3ff97237

--- Sources/TDLibKit/Generated/Models/ProfilePhoto.swift
+++ Sources/TDLibKit/Generated/Models/ProfilePhoto.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.7.10-7334b262
-//  https://github.com/tdlib/td/tree/7334b262
+//  Based on TDLib 1.7.10-3ff97237
+//  https://github.com/tdlib/td/tree/3ff97237
 //
 
 import Foundation
