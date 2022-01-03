08df35ff1c906c074d98720756c65c27f620f183
runner <runner@Mac-1641211474624.local>
2022-01-03 12:14:10 +0000

    [no ci] Version 1.2.0-tdlib-1.8.0-fa8feefe

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             targets: ["TDLibKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.8.0-c0385078")),
+        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.8.0-fa8feefe")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/TDLibKit/Generated/Models/GetGameHighScores.swift
+++ Sources/TDLibKit/Generated/Models/GetGameHighScores.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.0-c0385078
-//  https://github.com/tdlib/td/tree/c0385078
+//  Based on TDLib 1.8.0-fa8feefe
+//  https://github.com/tdlib/td/tree/fa8feefe
 //
 
 import Foundation
