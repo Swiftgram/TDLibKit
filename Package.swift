25df8a18d66c8c26a2ab9daf449ca8945ba5092d
runner <runner@Mac-1648124067434.local>
2022-03-24 12:16:06 +0000

    [no ci] Version 1.2.1-tdlib-1.8.2-461b7409

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             targets: ["TDLibKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.8.2-ff014add")),
+        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.8.2-461b7409")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/TDLibKit/Generated/Models/GetArchivedStickerSets.swift
+++ Sources/TDLibKit/Generated/Models/GetArchivedStickerSets.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.2-ff014add
-//  https://github.com/tdlib/td/tree/ff014add
+//  Based on TDLib 1.8.2-461b7409
+//  https://github.com/tdlib/td/tree/461b7409
 //
 
 import Foundation
