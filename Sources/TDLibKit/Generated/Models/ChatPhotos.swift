be0dcbf55f371361522bffcfae18ff31b147e362
runner <runner@Mac-1652702897449.local>
2022-05-16 12:20:02 +0000

    [no ci] Version 1.2.1-tdlib-1.8.3-b6c7da17

--- Sources/TDLibKit/Generated/Models/ChatPhotos.swift
+++ Sources/TDLibKit/Generated/Models/ChatPhotos.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.3-995b06b3
-//  https://github.com/tdlib/td/tree/995b06b3
+//  Based on TDLib 1.8.3-b6c7da17
+//  https://github.com/tdlib/td/tree/b6c7da17
 //
 
 import Foundation
