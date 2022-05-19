c3cbe244c5859e7a4f76cf4228f6367633d0b9e6
runner <runner@Mac-1652961735576.local>
2022-05-19 12:18:02 +0000

    [no ci] Version 1.2.1-tdlib-1.8.3-18cd0dd9

--- Sources/TDLibKit/Generated/Models/ReplaceVideoChatRtmpUrl.swift
+++ Sources/TDLibKit/Generated/Models/ReplaceVideoChatRtmpUrl.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.3-e65c0827
-//  https://github.com/tdlib/td/tree/e65c0827
+//  Based on TDLib 1.8.3-18cd0dd9
+//  https://github.com/tdlib/td/tree/18cd0dd9
 //
 
 import Foundation
