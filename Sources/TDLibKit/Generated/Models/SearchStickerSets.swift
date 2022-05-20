f43fe3c59c41f1760ce9965a94cef757129a3262
runner <runner@Mac-1653048722489.local>
2022-05-20 12:20:47 +0000

    [no ci] Version 1.2.1-tdlib-1.8.3-9c9a74c5

--- Sources/TDLibKit/Generated/Models/SearchStickerSets.swift
+++ Sources/TDLibKit/Generated/Models/SearchStickerSets.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.3-18cd0dd9
-//  https://github.com/tdlib/td/tree/18cd0dd9
+//  Based on TDLib 1.8.3-9c9a74c5
+//  https://github.com/tdlib/td/tree/9c9a74c5
 //
 
 import Foundation
