d51792fda728a1ed34a5106b2f1e39085b3e6b7c
runner <runner@Mac-1654131035675.local>
2022-06-02 00:57:32 +0000

    [no ci] Version 1.2.1-tdlib-1.8.3-3ae2ec38

--- Sources/TDLibKit/Generated/Models/RegisterUser.swift
+++ Sources/TDLibKit/Generated/Models/RegisterUser.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.3-c5dfa597
-//  https://github.com/tdlib/td/tree/c5dfa597
+//  Based on TDLib 1.8.3-3ae2ec38
+//  https://github.com/tdlib/td/tree/3ae2ec38
 //
 
 import Foundation
