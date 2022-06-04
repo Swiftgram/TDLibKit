5e2441e8c11992a88f117125989777f9efe15f11
runner <runner@Mac-1654303188090.local>
2022-06-04 00:47:21 +0000

    [no ci] Version 1.2.1-tdlib-1.8.3-e86b323d

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             targets: ["TDLibKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.8.3-3ae2ec38")),
+        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.8.3-e86b323d")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/TDLibKit/Generated/Models/ResendMessages.swift
+++ Sources/TDLibKit/Generated/Models/ResendMessages.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.3-3ae2ec38
-//  https://github.com/tdlib/td/tree/3ae2ec38
+//  Based on TDLib 1.8.3-e86b323d
+//  https://github.com/tdlib/td/tree/e86b323d
 //
 
 import Foundation
