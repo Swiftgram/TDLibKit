b10076c1714aaa8cde45a7036116cf495603ce3c
GGorAA <ggoraa1029@gmail.com>
2022-07-01 23:58:16 +0300

    feat: make run(updateHandler:) private

--- Sources/TDLibKit/TdClientImpl.swift
+++ Sources/TDLibKit/TdClientImpl.swift
@@ -45,27 +45,6 @@ open class TdClientImpl: TdClient {
         td_json_client_destroy(client)
     }
     
-    /// Receives incoming updates and request responses from the TDLib client
-    public func run(updateHandler: @escaping CompletionHandler) {
-        self.updateHandler = updateHandler
-        createClientIfNeeded()
-        
-        tdlibMainQueue.async { [unowned self] in
-            guard !self.isClientDestroyed else { return }
-            
-            while (!self.stopFlag) {
-                guard
-                    let res = td_json_client_receive(self.client, 10),
-                    let data = String(cString: res).data(using: .utf8)
-                else {
-                    continue
-                }
-                self.logger?.log(String(cString: res), type: .receive)
-                self.queryResultAsync(data)
-            }
-        }
-    }
-    
     /// Sends request to the TDLib client.
     public func send(query: TdQuery, completion: (CompletionHandler)? = nil) throws {
         guard !self.isClientDestroyed else { throw Error(code: 404, message: "Client destroyed") }
@@ -116,6 +95,27 @@ open class TdClientImpl: TdClient {
     }
     
     // MARK: - Private methods
+    
+    /// Receives incoming updates and request responses from the TDLib client
+    private func run(updateHandler: @escaping CompletionHandler) {
+        self.updateHandler = updateHandler
+        createClientIfNeeded()
+        
+        tdlibMainQueue.async { [unowned self] in
+            guard !self.isClientDestroyed else { return }
+            
+            while (!self.stopFlag) {
+                guard
+                    let res = td_json_client_receive(self.client, 10),
+                    let data = String(cString: res).data(using: .utf8)
+                else {
+                    continue
+                }
+                self.logger?.log(String(cString: res), type: .receive)
+                self.queryResultAsync(data)
+            }
+        }
+    }
     
     private func createClientIfNeeded() {
         if isClientDestroyed {
