7053eb87496fe0f688f9ba0c139e5713e61ee7bb
runner <runner@Mac-1657540890021.local>
2022-07-11 12:20:16 +0000

    [no ci] Version 1.2.1-tdlib-1.8.4-1dec0e20

--- Sources/TDLibKit/Generated/Models/VideoNote.swift
+++ Sources/TDLibKit/Generated/Models/VideoNote.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.4-d4890143
-//  https://github.com/tdlib/td/tree/d4890143
+//  Based on TDLib 1.8.4-1dec0e20
+//  https://github.com/tdlib/td/tree/1dec0e20
 //
 
 import Foundation
