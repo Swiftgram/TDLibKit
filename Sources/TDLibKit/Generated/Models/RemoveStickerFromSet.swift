04a2191624f4f6429bd2fdc341f85a0bc95b0641
runner <runner@Mac-1658231926462.local>
2022-07-19 12:20:00 +0000

    [no ci] Version 1.2.1-tdlib-1.8.4-87a51113

--- Sources/TDLibKit/Generated/Models/RemoveStickerFromSet.swift
+++ Sources/TDLibKit/Generated/Models/RemoveStickerFromSet.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.4-0147c97f
-//  https://github.com/tdlib/td/tree/0147c97f
+//  Based on TDLib 1.8.4-87a51113
+//  https://github.com/tdlib/td/tree/87a51113
 //
 
 import Foundation
