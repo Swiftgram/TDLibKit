2b0ea17a6eda0f19beafa37048b6fdd3a3acc67a
runner <runner@Mac-1658492054611.local>
2022-07-22 12:18:40 +0000

    [no ci] Version 1.2.1-tdlib-1.8.4-37647648

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             targets: ["TDLibKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.8.4-4ac8be81")),
+        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.8.4-37647648")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/TDLibKit/Generated/Models/SetProfilePhoto.swift
+++ Sources/TDLibKit/Generated/Models/SetProfilePhoto.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.4-4ac8be81
-//  https://github.com/tdlib/td/tree/4ac8be81
+//  Based on TDLib 1.8.4-37647648
+//  https://github.com/tdlib/td/tree/37647648
 //
 
 import Foundation
