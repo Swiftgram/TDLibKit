427178c805891312271a87e71dcbf6c44e8ce83c
runner <runner@Mac-1658837722896.local>
2022-07-26 12:18:46 +0000

    [no ci] Version 1.2.1-tdlib-1.8.4-9c6f4577

--- Sources/TDLibKit/Generated/Models/GetRecoveryEmailAddress.swift
+++ Sources/TDLibKit/Generated/Models/GetRecoveryEmailAddress.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.4-a63642c8
-//  https://github.com/tdlib/td/tree/a63642c8
+//  Based on TDLib 1.8.4-9c6f4577
+//  https://github.com/tdlib/td/tree/9c6f4577
 //
 
 import Foundation
