6d69e9b682983d9c319b75e5072f5af4c2dd35f6
runner <runner@Mac-1658923692700.local>
2022-07-27 12:23:29 +0000

    [no ci] Version 1.2.1-tdlib-1.8.4-6283dce8

--- Sources/TDLibKit/Generated/Models/InlineQueryResults.swift
+++ Sources/TDLibKit/Generated/Models/InlineQueryResults.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.4-9c6f4577
-//  https://github.com/tdlib/td/tree/9c6f4577
+//  Based on TDLib 1.8.4-6283dce8
+//  https://github.com/tdlib/td/tree/6283dce8
 //
 
 import Foundation
