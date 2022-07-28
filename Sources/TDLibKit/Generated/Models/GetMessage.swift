0695356eb724a686d9bbf12521e135355533bfb2
runner <runner@Mac-1659010787005.local>
2022-07-28 12:23:24 +0000

    [no ci] Version 1.2.1-tdlib-1.8.4-07b7faf6

--- Sources/TDLibKit/Generated/Models/GetMessage.swift
+++ Sources/TDLibKit/Generated/Models/GetMessage.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.4-6283dce8
-//  https://github.com/tdlib/td/tree/6283dce8
+//  Based on TDLib 1.8.4-07b7faf6
+//  https://github.com/tdlib/td/tree/07b7faf6
 //
 
 import Foundation
