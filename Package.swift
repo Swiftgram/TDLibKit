7f840b3ea3c325612e351ed24533b2e1907aaf85
runner <runner@Mac-1669377678862.local>
2022-11-25 12:18:31 +0000

    [no ci] Version 1.2.1-tdlib-1.8.8-3ec53c8d

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             targets: ["TDLibKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.8.8-2e6ac1f2")),
+        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.8.8-3ec53c8d")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/TDLibKit/Generated/Models/GetUserPrivacySettingRules.swift
+++ Sources/TDLibKit/Generated/Models/GetUserPrivacySettingRules.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.8-2e6ac1f2
-//  https://github.com/tdlib/td/tree/2e6ac1f2
+//  Based on TDLib 1.8.8-3ec53c8d
+//  https://github.com/tdlib/td/tree/3ec53c8d
 //
 
 import Foundation
