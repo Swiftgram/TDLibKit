bfaf84fe5120917b7d3f34ff3c4f29a27ab0b49b
runner <runner@Mac-1669682617641.local>
2022-11-29 00:46:12 +0000

    [no ci] Version 1.2.1-tdlib-1.8.8-6679837e

--- Sources/TDLibKit/Generated/Models/LanguagePackStrings.swift
+++ Sources/TDLibKit/Generated/Models/LanguagePackStrings.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.8-3ec53c8d
-//  https://github.com/tdlib/td/tree/3ec53c8d
+//  Based on TDLib 1.8.8-6679837e
+//  https://github.com/tdlib/td/tree/6679837e
 //
 
 import Foundation
