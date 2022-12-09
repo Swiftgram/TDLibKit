747e1d455a7826b9c527d4e43d03d0fca7457711
runner <runner@Mac-1670545554569.local>
2022-12-09 00:50:05 +0000

    [no ci] Version 1.2.1-tdlib-1.8.9-29752073

--- Sources/TDLibKit/Generated/Models/PassportElementType.swift
+++ Sources/TDLibKit/Generated/Models/PassportElementType.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.9-a7952f38
-//  https://github.com/tdlib/td/tree/a7952f38
+//  Based on TDLib 1.8.9-29752073
+//  https://github.com/tdlib/td/tree/29752073
 //
 
 import Foundation
