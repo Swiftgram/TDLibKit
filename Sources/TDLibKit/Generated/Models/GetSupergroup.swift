bb6116f69f6d6c00b5433a2d3bccc20de443c230
runner <runner@Mac-1673439016598.local>
2023-01-11 12:19:02 +0000

    [no ci] Version 1.2.1-tdlib-1.8.10-64406035

--- Sources/TDLibKit/Generated/Models/GetSupergroup.swift
+++ Sources/TDLibKit/Generated/Models/GetSupergroup.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.10-ed2644c2
-//  https://github.com/tdlib/td/tree/ed2644c2
+//  Based on TDLib 1.8.10-64406035
+//  https://github.com/tdlib/td/tree/64406035
 //
 
 import Foundation
