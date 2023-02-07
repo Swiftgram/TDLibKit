bab39677e5b3551536ef362cf5640541d0ab44cd
runner <runner@Mac-1675771957466.local>
2023-02-07 12:17:29 +0000

    [no ci] Version 1.2.1-tdlib-1.8.11-3d0140b3

--- Sources/TDLibKit/Generated/Models/User.swift
+++ Sources/TDLibKit/Generated/Models/User.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.11-5ed1d22d
-//  https://github.com/tdlib/td/tree/5ed1d22d
+//  Based on TDLib 1.8.11-3d0140b3
+//  https://github.com/tdlib/td/tree/3d0140b3
 //
 
 import Foundation
