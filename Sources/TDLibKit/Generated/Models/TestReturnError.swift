f1623455e261c0f22ad9e6c5fca960ae6e14bdad
runner <runner@Mac-1677199350630.local>
2023-02-24 00:44:25 +0000

    [no ci] Version 1.2.1-tdlib-1.8.11-1543c41f

--- Sources/TDLibKit/Generated/Models/TestReturnError.swift
+++ Sources/TDLibKit/Generated/Models/TestReturnError.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.11-6ba394fc
-//  https://github.com/tdlib/td/tree/6ba394fc
+//  Based on TDLib 1.8.11-1543c41f
+//  https://github.com/tdlib/td/tree/1543c41f
 //
 
 import Foundation
