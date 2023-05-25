2286cbb27772a05eeb2f51bc44f5db79df56dc2b
runner <runner@Mac-1684975130745.local>
2023-05-25 00:41:49 +0000

    [no ci] Version 1.2.1-tdlib-1.8.14-0150094c

--- Sources/TDLibKit/Generated/Models/StorageStatisticsByFileType.swift
+++ Sources/TDLibKit/Generated/Models/StorageStatisticsByFileType.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.14-90c36b60
-//  https://github.com/tdlib/td/tree/90c36b60
+//  Based on TDLib 1.8.14-0150094c
+//  https://github.com/tdlib/td/tree/0150094c
 //
 
 import Foundation
