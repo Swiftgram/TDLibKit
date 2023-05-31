8e73aec1bb1da363b08db6098d867e81a6e297b2
runner <runner@Mac-1685493429187.local>
2023-05-31 00:50:10 +0000

    [no ci] Version 1.2.1-tdlib-1.8.14-fe734fd6

--- Sources/TDLibKit/Generated/Models/InputChatPhoto.swift
+++ Sources/TDLibKit/Generated/Models/InputChatPhoto.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.14-54b34e91
-//  https://github.com/tdlib/td/tree/54b34e91
+//  Based on TDLib 1.8.14-fe734fd6
+//  https://github.com/tdlib/td/tree/fe734fd6
 //
 
 import Foundation
