3244cbf9d9dba7ef88521768fa9205e95979dd97
runner <runner@Mac-1685665958947.local>
2023-06-02 00:49:50 +0000

    [no ci] Version 1.2.1-tdlib-1.8.14-04525f6f

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             targets: ["TDLibKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.8.14-fe734fd6")),
+        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.8.14-04525f6f")),
     ],
     targets: [
         .target(
--- Sources/TDLibKit/Generated/Models/EditMessageText.swift
+++ Sources/TDLibKit/Generated/Models/EditMessageText.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.14-fe734fd6
-//  https://github.com/tdlib/td/tree/fe734fd6
+//  Based on TDLib 1.8.14-04525f6f
+//  https://github.com/tdlib/td/tree/04525f6f
 //
 
 import Foundation
