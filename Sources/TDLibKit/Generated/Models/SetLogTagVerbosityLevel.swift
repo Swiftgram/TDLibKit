3a30c982a78cd152ed9db264ecfc6b5d9a011b55
runner <runner@Mac-1686185113434.local>
2023-06-08 00:48:24 +0000

    [no ci] Version 1.2.1-tdlib-1.8.14-677219a2

--- Sources/TDLibKit/Generated/Models/SetLogTagVerbosityLevel.swift
+++ Sources/TDLibKit/Generated/Models/SetLogTagVerbosityLevel.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.14-4580cfc2
-//  https://github.com/tdlib/td/tree/4580cfc2
+//  Based on TDLib 1.8.14-677219a2
+//  https://github.com/tdlib/td/tree/677219a2
 //
 
 import Foundation
