6a2c5b9390cf65f838a40fa7b72884890b0b6e8a
runner <runner@Mac-1686485538939.local>
2023-06-11 12:14:48 +0000

    [no ci] Version 1.3.1-tdlib-1.8.14-1886bcf8

--- Sources/TDLibKit/Generated/Models/ViewPremiumFeature.swift
+++ Sources/TDLibKit/Generated/Models/ViewPremiumFeature.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.14-677219a2
-//  https://github.com/tdlib/td/tree/677219a2
+//  Based on TDLib 1.8.14-1886bcf8
+//  https://github.com/tdlib/td/tree/1886bcf8
 //
 
 import Foundation
