b50f65d05018cc1669da469cb33a3a9e47308149
runner <runner@Mac-1690978791952.local>
2023-08-02 12:28:46 +0000

    [no ci] Version 1.4.4-tdlib-1.8.15-8893dc80

--- Sources/TDLibKit/Generated/Models/MessageThreadInfo.swift
+++ Sources/TDLibKit/Generated/Models/MessageThreadInfo.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.15-6a6cd8af
-//  https://github.com/tdlib/td/tree/6a6cd8af
+//  Based on TDLib 1.8.15-8893dc80
+//  https://github.com/tdlib/td/tree/8893dc80
 //
 
 import Foundation
