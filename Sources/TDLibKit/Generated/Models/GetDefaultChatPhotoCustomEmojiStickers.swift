23e41e5f0b5f8bcc416bdac97d8bdd71724ee80c
runner <runner@Mac-1690991243019.local>
2023-08-02 15:50:26 +0000

    [no ci] Version 1.4.4-tdlib-1.8.15-ff687167

--- Sources/TDLibKit/Generated/Models/GetDefaultChatPhotoCustomEmojiStickers.swift
+++ Sources/TDLibKit/Generated/Models/GetDefaultChatPhotoCustomEmojiStickers.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.15-8893dc80
-//  https://github.com/tdlib/td/tree/8893dc80
+//  Based on TDLib 1.8.15-ff687167
+//  https://github.com/tdlib/td/tree/ff687167
 //
 
 import Foundation
