3d9de6f82d4c15e18e44db212f22a6c02c340ed8
runner <runner@Mac-1692187934370.local>
2023-08-16 12:15:34 +0000

    [no ci] Version 1.5.0-tdlib-1.8.16-db5d5da0

--- Sources/TDLibKit/Generated/Models/ToggleGroupCallIsMyVideoPaused.swift
+++ Sources/TDLibKit/Generated/Models/ToggleGroupCallIsMyVideoPaused.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.16-d44617b4
-//  https://github.com/tdlib/td/tree/d44617b4
+//  Based on TDLib 1.8.16-db5d5da0
+//  https://github.com/tdlib/td/tree/db5d5da0
 //
 
 import Foundation
