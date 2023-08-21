04f8ef91746d5b80d02b19088bcf2781d252eeab
runner <runner@Mac-1692619201443.local>
2023-08-21 12:25:24 +0000

    [no ci] Version 1.5.0-tdlib-1.8.16-4d1d22d6

--- Sources/TDLibKit/Generated/Models/DeleteLanguagePack.swift
+++ Sources/TDLibKit/Generated/Models/DeleteLanguagePack.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.16-203e8cf9
-//  https://github.com/tdlib/td/tree/203e8cf9
+//  Based on TDLib 1.8.16-4d1d22d6
+//  https://github.com/tdlib/td/tree/4d1d22d6
 //
 
 import Foundation
