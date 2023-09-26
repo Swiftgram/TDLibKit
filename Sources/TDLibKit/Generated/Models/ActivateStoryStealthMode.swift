eb193279a3afa78a49090ad18d82719062bcdc84
runner <runner@Mac-1695729810698.local>
2023-09-26 12:16:45 +0000

    [no ci] Version 1.5.0-tdlib-1.8.19-9b4266c0

--- Sources/TDLibKit/Generated/Models/ActivateStoryStealthMode.swift
+++ Sources/TDLibKit/Generated/Models/ActivateStoryStealthMode.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.19-0d16085d
-//  https://github.com/tdlib/td/tree/0d16085d
+//  Based on TDLib 1.8.19-9b4266c0
+//  https://github.com/tdlib/td/tree/9b4266c0
 //
 
 import Foundation
