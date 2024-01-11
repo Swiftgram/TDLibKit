39bccd22765350715e9107438c060320afb32fd6
runner <runner@Mac-1704974916361.local>
2024-01-11 12:16:43 +0000

    [no ci] Version 1.5.0-tdlib-1.8.23-94133602

--- Sources/TDLibKit/Generated/Models/CreateSupergroupChat.swift
+++ Sources/TDLibKit/Generated/Models/CreateSupergroupChat.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.23-d963044e
-//  https://github.com/tdlib/td/tree/d963044e
+//  Based on TDLib 1.8.23-94133602
+//  https://github.com/tdlib/td/tree/94133602
 //
 
 import Foundation
