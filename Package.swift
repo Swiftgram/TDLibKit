19402edf1cf98eed42c7db22232f362fcdb3cb95
runner <runner@Mac-1705060891977.local>
2024-01-12 12:15:51 +0000

    [no ci] Version 1.5.0-tdlib-1.8.23-4bafdc2b

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
             targets: ["TDLibKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.8.23-94133602")),
+        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.8.23-4bafdc2b")),
     ],
     targets: [
         .target(
--- Sources/TDLibKit/Generated/Models/KeyboardButton.swift
+++ Sources/TDLibKit/Generated/Models/KeyboardButton.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.23-94133602
-//  https://github.com/tdlib/td/tree/94133602
+//  Based on TDLib 1.8.23-4bafdc2b
+//  https://github.com/tdlib/td/tree/4bafdc2b
 //
 
 import Foundation
