5e2620fa20c52af3454dc679dacc23f2cd0f38b4
runner <runner@Mac-1705925651593.local>
2024-01-22 12:17:35 +0000

    [no ci] Version 1.5.0-tdlib-1.8.23-b4c07559

--- Sources/TDLibKit/Generated/Models/Hashtags.swift
+++ Sources/TDLibKit/Generated/Models/Hashtags.swift
@@ -3,8 +3,8 @@
 //  tl2swift
 //
 //  Generated automatically. Any changes will be lost!
-//  Based on TDLib 1.8.23-0f98d766
-//  https://github.com/tdlib/td/tree/0f98d766
+//  Based on TDLib 1.8.23-b4c07559
+//  https://github.com/tdlib/td/tree/b4c07559
 //
 
 import Foundation
